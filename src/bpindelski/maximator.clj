8ea2f391902b416b9cf6c28c2288b15c53169d41
Blazej Pindelski <blazej.pindelski@bloomdiagnostics.com>
2020-09-01 13:38:07 +0200

    feat: Adjust code to new MaxMind API

--- src/bpindelski/maximator.clj
+++ src/bpindelski/maximator.clj
@@ -1,4 +1,4 @@
-(ns com.bpindelski.maximator
+(ns bpindelski.maximator
   (:require [clojure.java.io :as io]
             [gavagai.core :as g])
   (:import [com.maxmind.geoip2 DatabaseReader$Builder]
@@ -26,22 +26,23 @@
   (g/register-converters
    {:exclude [:class]}
    [["com.maxmind.geoip2.model.CityResponse"]
+    ["com.maxmind.geoip2.model.CountryResponse"]
     ["com.maxmind.geoip2.model.ConnectionTypeResponse"]
     ["com.maxmind.geoip2.model.IspResponse"]
     ["com.maxmind.geoip2.model.DomainResponse"]
     ["com.maxmind.geoip2.record.City" :exclude [:names] :add {:names names-as-kw}]
     ["com.maxmind.geoip2.record.Continent" :exclude [:names] :add {:names names-as-kw}]
+    ["com.maxmind.geoip2.record.Country" :exclude [:names] :add {:names names-as-kw}]
     ["com.maxmind.geoip2.record.Subdivision" :exclude [:names] :add {:names names-as-kw}]
     ["com.maxmind.geoip2.record.RepresentedCountry" :exclude [:names] :add {:names names-as-kw}]
     ["com.maxmind.geoip2.record.Location"]
     ["com.maxmind.geoip2.record.MaxMind"]
     ["com.maxmind.geoip2.record.Postal"]
-    ["com.maxmind.geoip2.record.Country" :exclude [:names] :add {:names names-as-kw}]
-    ["com.maxmind.geoip2.record.Traits"]]))
+    ["com.maxmind.geoip2.record.Traits" :exclude [:network]]]))
 
 
 (defn make-locator
-  "Returns a locator from either a String file path, a File object or an InputStream. This locator is threadsafe.
+  "Returns a locator from either a String file path, a File object or an InputStream. This locator is thread-safe.
   Takes 2 optional arguments:
  + **:locales** List of recognised locales, for example [:en :fr], used for default :name
  + **:file-mode** Either *:memory-mapped* (default) or *:memory*"
@@ -53,11 +54,18 @@
         (cond-> file-mode (.fileMode (get file-modes file-mode)))
         (.build))))
 
-(defmulti lookup)
 
-
-(defn lookup
-  "Lookup an `ip` given as a String in the given `db` and returns a map with results"
+(defn lookup-city
+  "Lookup an `ip` given as a String in the given city `db` and returns a map with results.
+  Scoped to a city level."
   [^GeoIp2Provider db ip]
-  (if-let [city (.country db (InetAddress/getByName ip))]
+  (if-let [city (.city db (InetAddress/getByName ip))]
     (g/translate translator city {:lazy? false})))
+
+
+(defn lookup-country
+  "Lookup an `ip` given as a String in the given country `db` and returns a map with results.
+  Scoped to a country level."
+  [^GeoIp2Provider db ip]
+  (if-let [country (.country db (InetAddress/getByName ip))]
+    (g/translate translator country {:lazy? false})))
--- test/bpindelski/maximator_test.clj
+++ test/bpindelski/maximator_test.clj
@@ -1,10 +1,21 @@
-(ns com.bpindelski.maximator-test
+(ns bpindelski.maximator-test
+  "This test requires the *.mmdb files to be present in the test root folder.
+  Please download them from the MaxMind website."
   (:require [clojure.test :refer :all]
-            [com.bpindelski.maximator :refer :all]))
+            [bpindelski.maximator :refer :all]
+            [clojure.pprint :as pp]))
 
-(def loc (make-locator "test/GeoLite2-Country.mmdb" :locales [:en]))
 
-(deftest a-test
-  (println (lookup loc "8.8.8.8"))
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def locators {:city    (make-locator "test/GeoLite2-City.mmdb" :locales [:en])
+               :country (make-locator "test/GeoLite2-Country.mmdb" :locales [:de :fr])})
+
+
+(deftest basic-tests
+  (testing "city"
+    (let [city (lookup-city (:city locators) "2.21.172.132")]
+      (is (= "Vienna" (get-in city [:city :name])))
+      (is (= "AT" (get-in city [:country :iso-code])))))
+  (testing "country"
+    (let [country (lookup-country (:country locators) "8.8.8.8")]
+      (is (= "US" (get-in country [:country :iso-code])))
+      (pp/pprint country))))
